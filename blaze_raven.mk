737ca6aab1b737527ee471427da62ab5390c5e7d
Michael Bestas <mkbestas@lineageos.org>
2024-07-03 18:18:57 +0300

    Update fingerprint/build description from AP2A.240705.004
    Change-Id: Ia27bb7f823b93d9aeb0c5fea1b6efacae80fd7e9

--- blaze_raven.mk
+++ blaze_raven.mk
@@ -33,8 +33,8 @@ WITH_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 14 AP2A.240605.024 11860263 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 14 AP2A.240705.004 11875680 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:14/AP2A.240605.024/11860263:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:14/AP2A.240705.004/11875680:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
