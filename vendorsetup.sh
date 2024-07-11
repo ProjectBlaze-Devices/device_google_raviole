a5d28a99df71de3b0ae882340c75706f3ab4f605
Crypted <sami.mhatre756@gmail.com>
2024-07-11 23:55:04 +0530

    raviole: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://gitlab.com/CYB3R0ID694/proprietary_vendor_google_oriole.git -b A14-blaze vendor/google/oriole
+git clone https://gitlab.com/CYB3R0ID694/proprietary_vendor_google_raven.git -b A14-blaze vendor/google/raven
+git clone https://android.googlesource.com/device/google/raviole-kernel.git --depth=1 device/google/raviole-kernel
+git clone https://github.com/ProjectBlaze-Devices/device_google_gs101.git -b 14 device/google/gs101
+git clone https://gitlab.com/CYB3R0ID694/vendor_gcam.git -b CYB3R0ID694-main-patch-47042 vendor/gcam
